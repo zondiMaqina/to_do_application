0ee3c2ab5e4b6dc4782d5dd64a5868094cd4b8b7
zondimaqina <zondimaqina0@gmail.com>
2024-06-25 15:03:11 +0200

    add welcome stage for user

--- lib/user_interact.rb
+++ lib/user_interact.rb
@@ -3,7 +3,27 @@ require 'colorize'
 class UserInteraction
   def initialize
     puts 'Hello there User kindly!!! supply us with your details'.upcase.colorize(:color => :yelow, :background => :blue)
-  end
+    sleep(3)
+    puts 'Open application (Yes/No)'
+    user_answer = gets.chomp
+
+    until user_answer.downcase == 'yes' || user_answer.downcase == 'no'
+      puts 'Please enter YES or NO'.colorize(:red)
+      user_answer = gets.chomp
+    end
+
+    if user_answer == 'yes'
+      personal_details
+    else
+        puts 'Thank you for your participence'.colorize(:green)
+    end
 end
 
-UserInteraction.new
+private
+
+def personal_details
+    puts 'please enter your name and surname'
+end
+end
+
+

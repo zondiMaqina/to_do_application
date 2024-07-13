884619a8b3530d35c44c257762d1cf7b64a3e1d0
zondimaqina <zondimaqina0@gmail.com>
2024-07-13 17:17:49 +0200

    add method for intro

--- lib/user_interact.rb
+++ lib/user_interact.rb
@@ -2,28 +2,41 @@ require 'colorize'
 # class creation for terminal user interface
 class UserInteraction
   def initialize
-    puts 'Hello there User kindly!!! This is your to do list app :)'.upcase.colorize(:color => :yelow, :background => :blue)
+    until user_response.downcase == 'yes' || user_response.downcase == 'no'
+      puts 'Please enter YES or NO'.colorize(:red)
+      user_response = gets.chomp
+    end
+
+    if user_response == 'yes'
+      user_checker
+    else
+      puts 'Thank you for using the application :)'.colorize(:green)
+    end
+  end
+
+  private
+
+  def user_response
+    puts 'Hello there User kindly!!! This is your to do list app :)'.upcase.colorize(color: :yelow,
+                                                                                     background: :blue)
     sleep(3)
     puts 'Open application (Yes/No)'
+    gets.chomp
+  end
+
+  def user_checker
+    puts 'Is this YOUR FIRST TIME using this application?'.colorize(color: :yellow)
     user_answer = gets.chomp
 
     until user_answer.downcase == 'yes' || user_answer.downcase == 'no'
-      puts 'Please enter YES or NO'.colorize(:red)
+      puts 'Please answer a yes or no :('.colorize(color: :red)
       user_answer = gets.chomp
     end
 
-    if user_answer == 'yes'
+    if user_answer.downcase == 'yes'
       personal_details
     else
-        puts 'Thank you for using the application :)'.colorize(:green)
+      puts 'What would you like to do :)'.colorize(color: :orange)
     end
+  end
 end
-
-private
-
-def personal_details
-    puts 'please enter your name and surname'
-end
-end
-
-
